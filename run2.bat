59e55e8b054f4cd8e6e2e795e24be0db3f356245
pvzzombs <pvzzombs@gmail.com>
2021-03-06 18:29:50 +0800

    traverse vs19 2

--- run2.bat
+++ run2.bat
@@ -6,10 +6,10 @@ if "%~1"=="" echo First arg must be target directory& goto :eof
 if exist "c:\Program Files (x86)\." set "x86= (x86)"& set w32=syswow64
 dir "c:\Program Files%x86%\Microsoft Visual Studio\2019\Community\Common7\IDE"
 dir "c:\Program Files%x86%\Microsoft Visual Studio\2019\Community\Common7\IDE\1033"
-dir "c:\Program Files%x86%\Microsoft Visual Studio\2019\Community\Common7\IDE\VC"
+dir "c:\Program Files%x86%\Microsoft Visual Studio\2019\Community\Common7\IDE\VC\bin"
 dir "c:\Program Files%x86%\Microsoft Visual Studio\2019\Community\MSBuild\Sdks"
 dir "c:\Program Files%x86%\Microsoft Visual Studio\2019\Community\SDK"
-dir "c:\Program Files%x86%\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC"
+dir "c:\Program Files%x86%\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29333"
 dir "c:\Program Files%x86%\Microsoft Visual Studio\2019\Community\VSSDK"
 set vs1=c:\Program Files%x86%\Microsoft Visual Studio\2019\Community
 rem dir "%vs1%"
