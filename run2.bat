3c137a91ce7380212907a2b752db68f4125c40be
pvzzombs <pvzzombs@gmail.com>
2021-03-07 19:38:11 +0800

    vs19 1 - rewind

--- run2.bat
+++ run2.bat
@@ -13,22 +13,19 @@ set vs2=c:\Program Files%x86%\Windows Kits\10
 rem dir "%vs2%"
 set vs3=c:\Program Files%x86%\Windows Kits\8.1
 rem dir "%vs3%"
-set vs4=c:\Program Files%x86%\Microsoft Visual Studio 14.0
-rem dir "%vs4%"
 
 set emsg=Can't find src dir - run on system where compiler is installed
-if exist "%vs1%" if exist "%vs2%" if exist "%vs3%" if exist "%vs4%" set emsg=
+if exist "%vs1%" if exist "%vs2%" if exist "%vs3%" set emsg=
 if not "%emsg%"=="" echo %emsg%& goto :eof
 
 if exist "%~1" echo dir "%~1" exists... delete it first& goto :eof
 
 rem rem dir "c:\windows\%w32%"
 echo Copying binaries (bin, sdk\bin)..........
-xcopy /i /y "%vs1%\VC\Tools\MSVC\14.28.29333\bin\Hostx86\x86" "%~1\bin" >nul
+xcopy /i /y "%vs1%\VC\Tools\MSVC\14.28.29333\bin\Hostx86\x86" "%~1\bin\x86" >nul
 xcopy /i /y "%vs1%\VC\Tools\MSVC\14.28.29333\bin\Hostx86\x86" "%~1\bin\x64" >nul
 rem xcopy /i "%vs1%\VC\bin" "%~1\bin" >nul
-rem xcopy /i "%vs1%\VC\bin\1033" "%~1\bin\1033" >nul
-xcopy /i "%vs4%\VC\bin\1033" "%~1\bin\1033" >nul
+xcopy /i "%vs1%\VC\bin\1033" "%~1\bin\1033" >nul
 rem xcopy /i /y "%vs1%\Common7\IDE\1033" "%~1\bin\1033" >nul
 rem xcopy /i "%vs1%\VC\bin\x86_amd64" "%~1\bin\x86_amd64" >nul
 rem xcopy /i "%vs1%\VC\bin\x86_amd64\1033" "%~1\bin\x86_amd64\1033" >nul
@@ -40,13 +37,13 @@ rem xcopy /i /y /f "%vs3%\Debuggers\x86" "%~1\bin\Debuggers\x86\" >nul
 rem xcopy /i /y /f "%vs2%\Debuggers\x86" "%~1\bin\Debuggers\x86\" >nul
 rem xcopy /i /y /f "%vs3%\Debuggers\x64" "%~1\bin\Debuggers\x64\" >nul
 rem xcopy /i /y /f "%vs2%\Debuggers\x64" "%~1\bin\Debuggers\x64\" >nul
-rem xcopy /y c:\windows\system32\api-ms-win-*.dll "%~1\bin" >nul
-xcopy /y "%vs1%\Common7\IDE\api-ms-win-*.dll" "%~1\bin" >nul
+xcopy /y c:\windows\system32\api-ms-win-*.dll "%~1\bin" >nul
+xcopy /y "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\api-ms-win-*.dll" "%~1\bin" >nul
 xcopy /y c:\windows\%w32%\ucrtbase*.dll "%~1\bin" >nul
 xcopy /y c:\windows\%w32%\*140*.dll "%~1\bin" >nul
 xcopy /y c:\windows\%w32%\VsGraphicsHelper.dll "%~1\bin" >nul
-xcopy /y "%vs1%\Common7\IDE\*140*.dll" "%~1\bin" >nul
-xcopy /y "%vs1%\Common7\IDE\ucrtbase*.dll" "%~1\bin" >nul
+xcopy /y "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\*140*.dll" "%~1\bin" >nul
+xcopy /y "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\ucrtbase*.dll" "%~1\bin" >nul
 rem 
 rem rem dir "%vs2%\include"
 echo Copying includes (include, sdk\include)..........
@@ -60,7 +57,7 @@ rem rem dir "%vs1%\lib"
 rem rem dir "%vs2%\lib"
 echo Copying libraries (lib, sdk\lib)..........
 rem xcopy /i "%vs1%\VC\lib" "%~1\lib" >nul
-xcopy /i /y "%vs1%\VC\Tools\MSVC\14.28.29333\lib\x86" "%~1\lib" >nul
+xcopy /i /y "%vs1%\VC\Tools\MSVC\14.28.29333\lib\x86" "%~1\lib\x86" >nul
 xcopy /i /y "%vs1%\VC\Tools\MSVC\14.28.29333\lib\x64" "%~1\lib\x64" >nul
 xcopy /i /y "%vs2%\lib\10.0.19041.0\ucrt\x86" "%~1\lib" >nul
 xcopy /i "%vs3%\lib\winv6.3\um\x86" "%~1\sdk\lib" >nul
